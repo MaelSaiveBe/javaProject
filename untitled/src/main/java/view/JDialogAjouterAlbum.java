9fa12361c193ccbb71593527d4092a5b18ecb905
MaelSaive <66523278+MaelSaive@users.noreply.github.com>
2025-06-11 19:02:08 +0200

    ajoutform Ajout Album

---
+++ untitled/src/main/java/view/JDialogAjouterAlbum.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class JDialogAjouterAlbum {
+    private JTextField entrerLeTitreDeTextField;
+    private JTextField entrerLeNomDuTextField;
+    private JTextField listesDesMembresEntreTextField;
+    private JSpinner DateDays;
+    private JSpinner DateMonth;
+    private JSpinner DateYear;
+}
